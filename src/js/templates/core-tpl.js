86b2643568e23b953940ba6984fcc9370ef538df
dylan275 <dylan.motard@etu.univ-tours.fr>
2021-02-07 02:35:10 +0100

    View HTML core

--- src/js/templates/core-tpl.js
+++ src/js/templates/core-tpl.js
@@ -2,8 +2,14 @@ import * as Handlebars from 'handlebars';
 
 let core_tpl = `
 <div id="coreHeader">
-    <h4 id="ViewCoreLabel">View Core</h4>
-    <button type="button" class="btn btn-sm btn-primary" id="cb-vc-download"><i class="fa fa-download"></i> Download Core</button>
+    <div id="coreSubHeader">
+        <h4 id="ViewCoreLabel">View Core</h4>
+        <button type="button" class="btn btn-sm btn-primary" id="cb-vc-download"><i class="fa fa-download"></i> Download Core</button>
+    </div>
+    <button type="button" class="btn btn-secondary" id="toggling">
+        <i class="fa fa-code"></i>
+        <span>HTML</span>
+    </button>
 </div>
 <div class="cb-XMLFile"></div>
 `
